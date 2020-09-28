d5a59c515f83535a1e4d540788b793437cc9f7c9
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-28 15:58:58 -0700

    make splash page look prettier
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import cocktail from './img/cocktails.jpeg';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 
 function SplashPage() {
   return(
@@ -12,17 +15,39 @@ function SplashPage() {
         <Card.Img src={cocktail} alt="Some Cocktails" />
         <Card.ImgOverlay>
           <Card.Body>
-            <Card.Text className="about-cocktails">
+            <div className="about-cocktails">
               <h3>About Cocktails</h3>
-            </Card.Text>
-            <Card.Text className="testimonials">
+              <Row>
+                <Col>
+                  <h5>Meet new and interesting cocktails!</h5>
+                </Col>
+                <Col>
+                  <h5>So easy to use!</h5>
+                </Col>
+                <Col>
+                  <h5>We have all the greatest cocktails!</h5>
+                </Col>
+              </Row>
+            </div>
+            <div className="testimonials">
               <h3>Everybody loves cocktails!</h3>
-            </Card.Text>
+              <Carousel indicators={false} controls={false} fade={true} keyboard={false}>
+                <Carousel.Item interval={1000}>
+                  <p><strong>I love drinking!</strong></p>
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                  <p><strong>It numbs the pain!</strong></p>
+                </Carousel.Item>
+                <Carousel.Item interval={1000}>
+                  <p><strong>For thirty seconds, I don't feel like dying!</strong></p>
+                </Carousel.Item>
+              </Carousel>
+            </div>
           </Card.Body>
           <Card.Footer>
-            <Card.Text className="copyright-footer">
-              <h4>Copyright details</h4>
-            </Card.Text>
+            <div className="copyright-footer">
+              <h4>© Copyright 2020 Micheal Hansen, Ben Russell</h4>
+            </div>
           </Card.Footer>
         </Card.ImgOverlay>
       </Card>
